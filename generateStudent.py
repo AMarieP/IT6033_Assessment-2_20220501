4807c8dd62c9caed3e449650a30d504931ac5645
Alyssa <alyssa@kvasl.com>
2023-03-31 16:09:39 +1300

    Starting out with a student class

---
+++ generateStudent.py
@@ -0,0 +1,14 @@
+#Class which loads student data from DB, makes ID number if not avalibel yet& writes to DB
+
+class Student:
+    def __init__(self, firstName, lastName, email, campus) -> None:
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.campus = campus
+        self.GenerateID
+
+    def GenerateID():
+        pass
+
+    
