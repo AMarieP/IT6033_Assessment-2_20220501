0eb996d66897a3bc9b53bf90b561c0d9051673a3
Alyssa <alyssa@kvasl.com>
2023-04-01 14:29:02 +1300

    Playing sround with how to store my student data

---
+++ students.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, fname, lname, email, campus, studentID) -> None:
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.campus = campus
+        self.studentID = studentID
+
+#the list of all the students
+class Students:
+    def __init__(self, value) -> None:
+        pass
